771e9ccb187c6d9e349be15be54b14a58bc4f755
lizhipeng <lizhipeng@ugirls.com>
2018-08-24 17:15:40 +0800

    新增BaseActivity的demo

---
+++ app/src/main/java/com/lzp/baseui/ExtendBaseActivity.kt
@@ -0,0 +1,44 @@
+package com.lzp.baseui
+
+import android.view.Window
+import android.widget.TextView
+import android.widget.Toast
+import com.lzp.baseui.activity.BaseActivity
+
+class ExtendBaseActivity : BaseActivity() {
+
+    private val lifeCycleBean = LifeCycleBean(this)
+
+    override fun setWindowFeature() {
+        super.setWindowFeature()
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setShowBackTip(true)
+    }
+
+    override fun initView() {
+        setContentView(R.layout.activity_extend_base)
+    }
+
+    override fun loadData() {
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "BaseActivity"
+        // 为LifeCycleBean绑定Activity的周期
+        addLifeCycleObserver(lifeCycleBean)
+    }
+
+    override fun showBackTip() {
+        super.showBackTip()
+        Toast.makeText(this, "再按一次退出页面", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun confirmBack() {
+        super.confirmBack()
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(lifeCycleBean)
+    }
+
+}
--- app/src/main/java/com/lzp/baseui/MainActivity.kt
+++ app/src/main/java/com/lzp/baseui/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.lzp.baseui
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.baseactivity).setOnClickListener {
+            startActivity(Intent(this@MainActivity, ExtendBaseActivity::class.java))
+        }
+
     }
 }
